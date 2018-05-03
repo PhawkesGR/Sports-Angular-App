142d9b33c533dff9d3e5a5e371901af423e237a1
Dimitris Katomeris <jimkatompao@gmail.com>
2018-05-03 20:12:27 +0300

    done, pending deploy to heroku

---
+++ script.js
@@ -0,0 +1,69 @@
+angular.module('sportsApp', []).controller('mainController', function ($scope, $http, $filter) {
+
+  //get current year
+  var today = new Date();
+  var yyyy = today.getFullYear();
+  
+  $scope.show = false;
+  $scope.image = false;
+  var age;
+  
+  //call API
+  $http({
+    method: 'GET',
+    url: 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?t=West',
+    data: $scope.data
+  }).then(function successCallback(response) {
+      console.log(response.data.player);
+     
+      $scope.myData = {};
+      $scope.myData.players = response.data.player;
+      console.log($scope.myData.players);
+      //when a player's name is clicked, show the details
+      $scope.myData.playerClick = function(player) {
+          //parse the year the player was born and substract from current year to find age
+          age = parseInt(player.dateBorn);
+          $scope.age = yyyy-age;
+          $scope.description = player.strDescriptionEN;
+          $scope.name = player.strPlayer;
+          $scope.photo = player.strThumb;
+          $scope.fee = Number(player.strSigning.replace(/[^0-9\.-]+/g,""));
+          //json is not complete for all players, so precautions just in case something is empty
+          if($scope.description==null){
+            $scope.description= "No description available";
+          }
+          if($scope.photo==null){
+            $scope.image == true;
+          }
+          if($scope.fee==null){
+            $scope.fee = "Transfer fee not known";
+          }
+          $scope.show = true;
+
+          
+      }
+
+      //make favorite
+      var favs =[];
+      $scope.myData.makeFav = function(){
+        // $scope.myData.favorites.push(player);
+        console.log($scope.name);
+        favs.push($scope.name);
+        $scope.myData.favorites = favs;
+      }
+
+      //unfavorite
+      $scope.myData.unFav = function(){
+        favs.pop($scope.name);
+      }
+
+  }, function errorCallback(response) {
+
+  });
+
+  
+  
+
+
+  
+})
